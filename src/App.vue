c78821b3ee849fec47f95e4b3c39bf7890bad8a9
eXsik <qba.skupinski@gmail.com>
2024-02-05 14:07:37 +0100

    working on tracker expesnes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,58 @@
 <script setup>
 import Header from '@/components/Header.vue';
 import Balance from '@/components/Balance.vue';
+import IncomeExpenses from '@/components/IncomeExpenses.vue';
+import TransactionList from '@/components/TransactionList.vue';
+import AddTransaction from '@/components/AddTransaction.vue';
+
+import { computed, ref } from 'vue';
+
+const transactions = ref([
+  { id: 1, text: 'Flowers', amount: -19.99 },
+  { id: 2, text: 'Salary', amount: 299.97 },
+  { id: 3, text: 'Book', amount: -10.00 },
+  { id: 4, text: 'Camera', amount: 150.00 }
+]);
+
+// Get total
+const total = computed(() => {
+  return transactions.value
+    .reduce((accumulator, transaction) => {
+      return accumulator + transaction.amount;
+    }, 0);
+});
+
+// Get income
+const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((accumulator, transaction) => {
+      return accumulator + transaction.amount;
+    }, 0).toFixed(2);
+});
+
+// Get expenses
+const expenses = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((accumulator, transaction) => {
+      return accumulator + transaction.amount;
+    }, 0).toFixed(2);
+});
+
+// Add Transaction
+const handleTransactionSubmitted = (transactionData) => {
+  console.log(transactionData);
+};
+
 </script>
 
 <template>
   <Header></Header>
-  <Balance></Balance>
-  My App
-
+  <div class="container">
+    <Balance :total="+total"></Balance>
+    <IncomeExpenses :income="+income" :expenses="+expenses"></IncomeExpenses>
+    <TransactionList :transactions="transactions"></TransactionList>
+    <AddTransaction @transaction-submitted="handleTransactionSubmitted"></AddTransaction>
+  </div>
 </template>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+    transactions: {
+        type: Array,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+        <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
+            {{ transaction.text }} <span>{{ transaction.amount }}</span><button class="delete-btn">X</button>
+        </li>
+    </ul>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
